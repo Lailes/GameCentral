edbbf0962a92e5b02f799a5e999b05d4fd61a1e9
Auriel <amadera@protonmail.com>
2020-11-06 14:09:38 +0300

    Kafka init

---
+++ GameCentral.DatabaseService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using GameCentral.Shared.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameCentral.DatabaseService {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length == 0 || !int.TryParse(args[0], out _)) return;
+            
+            Console.WriteLine("GameCentral Database Kafka Service");
+            Console.WriteLine("Port: " + args[0]);
+            Console.WriteLine("Ctrl + C to exit");
+            Console.WriteLine("=====================================");
+            
+            var source = new CancellationTokenSource();
+            var token = source.Token;
+            Console.CancelKeyPress += delegate { source.Cancel(); };
+
+            var options = new DbContextOptionsBuilder<GameCentralContext>();
+            options.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=GameCentral;Trusted_Connection=True;Integrated Security=True");
+            using var context = new GameCentralContext(options.Options);
+            var repository = new EfGameCentralRepository(context);
+            
+            
+            
+            
+        }
+    }
+}
---
+++ KafkaProducerLine/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Confluent.Kafka;
+
+
+namespace KafkaProducerLine {
+    class Program {
+        private const string TopicName = "GameCentralMessages";
+        
+        static void Main(string[] args) {
+            var config = new ProducerConfig {
+                BootstrapServers = "localhost:9092"
+            };
+
+            using var producer1 = new ProducerBuilder<string, string>(config).Build();
+            using var producer2 = new ProducerBuilder<string, string>(config).Build();
+
+            for (int i = 0; i < 10; i++) {
+                producer1.Produce(TopicName, new Message<string, string> {
+                    Value = (i).ToString(),
+                    Key = (-i).ToString()
+                });
+            }
+
+            producer1.Flush();
+            for (int i = 100; i < 110; i++) {
+                producer2.Produce(TopicName, new Message<string, string> {
+                    Value = (i).ToString(),
+                    Key = (-i).ToString()
+                });
+            }
+
+            producer2.Flush();
+
+        }
+    }
+}
